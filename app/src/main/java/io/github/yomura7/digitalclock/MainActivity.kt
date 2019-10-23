ffd1d189c90f92abee29369ade4dc5866e1c6297
Yohei Omura <yomura7@gmail.com>
2019-10-23 16:44:51 +0900

    add second timer

--- app/src/main/java/io/github/yomura7/digitalclock/MainActivity.kt
+++ app/src/main/java/io/github/yomura7/digitalclock/MainActivity.kt
@@ -1,14 +1,20 @@
 package io.github.yomura7.digitalclock
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var secondTimer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +23,29 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
+
+        val textView = findViewById<TextView>(R.id.current_time)
+        var handler = Handler()
+        secondTimer = Timer().apply {
+            val timerTask = object: TimerTask() {
+                override fun run(){
+                    handler.post {
+                        Log.d("app", "hello")
+                        textView.apply {
+                            text = showCurrentTime()
+                        }
+                    }
+                }
+            }
+            schedule(timerTask, 1000, 1000)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        secondTimer.cancel()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,9 +58,11 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun showCurrentTime(): String = Date().toString()
 }
