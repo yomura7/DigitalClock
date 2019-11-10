655ede36d90ad8b7961b6625c8d485126ee0dc97
Yohei Omura <yomura7@gmail.com>
2019-11-11 00:07:35 +0900

    add setting spinners

--- app/src/main/java/io/github/yomura7/digitalclock/MainActivity.kt
+++ app/src/main/java/io/github/yomura7/digitalclock/MainActivity.kt
@@ -1,5 +1,7 @@
 package io.github.yomura7.digitalclock
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -7,6 +9,10 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,7 +20,7 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var secondTimer: Timer
 
@@ -42,6 +48,68 @@ class MainActivity : AppCompatActivity() {
             }
             schedule(timerTask, 1000, 1000)
         }
+
+        // Spinner1
+        val textSizeSpinner = findViewById<Spinner>(R.id.text_size_spinner)
+        textSizeSpinner.onItemSelectedListener = this
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.font_size_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            textSizeSpinner.adapter = adapter
+        }
+
+        // TODO: Spinner2
+        val textFontSpinner = findViewById<Spinner>(R.id.text_font_spinner)
+        textFontSpinner.onItemSelectedListener = this
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.text_font_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            textFontSpinner.adapter = adapter
+        }
+
+        // Spinner3
+        val textColorSpinner = findViewById<Spinner>(R.id.text_color_spinner)
+        textColorSpinner.onItemSelectedListener = this
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.text_color_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            textColorSpinner.adapter = adapter
+        }
+
+        // Spinner4
+        val backgroundColorSpinner = findViewById<Spinner>(R.id.background_color_spinner)
+        backgroundColorSpinner.onItemSelectedListener = this
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.background_color_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            backgroundColorSpinner.adapter = adapter
+        }
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        val textView = findViewById<TextView>(R.id.current_time)
+        val item = parent.getItemAtPosition(pos) as? String ?: return
+        when (parent.id) {
+            R.id.text_size_spinner -> textView.textSize = item.toFloat()
+            R.id.text_color_spinner -> textView.setTextColor(Color.parseColor(item))
+            R.id.text_font_spinner -> textView.typeface = Typeface.create(item, Typeface.NORMAL)
+            R.id.background_color_spinner -> this.window.decorView.setBackgroundColor(Color.parseColor(item))
+        }
+    }
+    override fun onNothingSelected(parent: AdapterView<*>) {
     }
 
     override fun onDestroy() {
